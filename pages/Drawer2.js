bd4b2a7f2f88b36dd2aa7daedf83a3dc0fbbf6b6
juguarnieri <juliaguarnieri04@gmail.com>
2025-03-26 17:28:54 -0300

    refactor: color pink Drawer2

--- pages/Drawer2.js
+++ pages/Drawer2.js
@@ -23,7 +23,7 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
-        backgroundColor: "yellow",
+        backgroundColor: "pink",
         padding: 20,
     },
     image: {
